ae372fffd43b1d12993687831eebcbc258a63cd2
CB-Pantomime <christopherbrown.contact@gmail.com>
2023-03-25 21:49:12 -0700

    build

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='home-main'>
+    <a href='/'>
+        <img 
+        src='https://res.cloudinary.com/dxov7pk4a/image/upload/v1679804549/A8D65893-23AB-4748-B44F-F920D9711B7A_1_105_c_w49fu2.jpg'
+        className='home-link-logo'
+        alt='man relaxing in kitchen'
+        ></img>
+        </a>
+    <h2 className='home-title'>we done bonked yr mom and made a podcast not about that.</h2>
+    <p>-Kerry & Claire</p>
+    <h2 className='home-blurb'>
+      <strong>books.</strong>
+      </h2>
+      <h3 className='home-blurb-comingsoon'>coming soon this very spring.</h3>
+   </div>
+  );
+} 
+export default App;
